ed2759b1aaef149a390479944019dcefbca548e5
the8bitbass <the8bitbass@gmail.com>
2026-09-18 12:46:11 +1000

    Update obsidian plugin config

--- lua/the8bitbass/lazy/obsidian.lua
+++ lua/the8bitbass/lazy/obsidian.lua
@@ -27,18 +27,20 @@ local getInput = function(prompt, opts)
     end
 end
 
+local newFromTemplate = function(template)
+    local title = getInput("Enter " .. template .. " title or path (optional):", { completion = "file" })
+    if title == nil then
+        return
+    end
+
+    require("obsidian.actions").new_from_template(title, template, function(note) note:open({ sync = true }) end)
+end
+
 return {
-    "the8bitbass/obsidian.nvim",
-    dev = true,
-    branch = "main",
+    "obsidian-nvim/obsidian.nvim",
+    version = "*",
     -- lazy = true,
     -- ft = "markdown",
-    dependencies = {
-        -- Required.
-        "nvim-lua/plenary.nvim",
-        "hrsh7th/nvim-cmp",
-        "nvim-telescope/telescope.nvim",
-    },
     opts = {
         workspaces = {
             {
@@ -47,43 +49,37 @@ return {
             },
         },
         legacy_commands = false,
-        archive_subdir = "Archive",
         notes_subdir = "Inbox",
-        note_path_func = function(spec)
+        note_id_func = function(title, path)
             local function camelize(input)
                 return input
                     :gsub("(%s)(%a)", function(_, letter) return letter:upper() end) -- uppercase letters after spaces
                     :gsub("^%a", string.upper) -- uppercase the first character if it's a letter
                     :gsub("%s+", "") -- remove all spaces
             end
-            if spec.title then
-                local path = spec.dir / camelize(tostring(spec.title))
-                return path:with_suffix(".md")
-            else
-                -- This is equivalent to the default behavior.
-                local path = spec.dir / tostring(spec.id)
-                return path:with_suffix(".md")
+
+            if title and title ~= "" then
+                return camelize(title)
             end
+
+            return require("obsidian.builtin").zettel_id(title, path)
         end,
-        statusline = {
+        footer = {
             enabled = false,
         },
-        disable_frontmatter = function(filename)
-            print(filename)
-            local fn = string.lower(filename)
-            if string.find(fn, "shoppinglist") or string.find(fn, "inbox") then
-                return true
-            else
-                return false
-            end
-        end,
-        -- Optional, for templates (see https://github.com/obsidian-nvim/obsidian.nvim/wiki/Using-templates)
+        frontmatter = {
+            enabled = function(filename)
+                local name = string.lower(filename or "")
+                return not name:find("shoppinglist", 1, true) and not name:find("inbox", 1, true)
+            end,
+        },
+        -- Optional, for templates (see https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template)
         templates = {
             folder = "Templates",
             date_format = "%Y-%m-%d",
             time_format = "%H:%M",
             -- A map for custom variables, the key should be the variable and the value a function.
-            -- Functions are called with obsidian.TemplateContext objects as their sole parameter.
+            -- Functions receive an obsidian.TemplateContext and an optional format suffix.
             -- See: https://github.com/obsidian-nvim/obsidian.nvim/wiki/Template#substitutions
             substitutions = {},
 
@@ -142,27 +138,27 @@ return {
         },
         -- Optional, define your own callbacks to further customize behavior.
         callbacks = {
-            post_setup = function(client)
+            post_setup = function()
                 vim.keymap.set( "n", "<leader>on", function() vim.cmd("Obsidian new") end, { desc = "New [N]ote" })
                 vim.keymap.set( "n", "<leader>of", function() vim.cmd("Obsidian new_from_template") end, { desc = "New note [F]rom template" })
-                vim.keymap.set( "n", "<leader>op", function() vim.cmd("Obsidian new_from_template contact") end, { desc = "New [P]erson" })
-                vim.keymap.set( "n", "<leader>oi", function() vim.cmd("Obsidian new_from_template idea") end, { desc = "New [I]dea" })
-                vim.keymap.set( "n", "<leader>or", function() vim.cmd("Obsidian new_from_template recipe") end, { desc = "New [R]ecipe" })
-                vim.keymap.set( "n", "<leader>ot", function() vim.cmd("Obsidian new_from_template topic") end, { desc = "New [T]opic" })
-                vim.keymap.set( "n", "<leader>occ", function() vim.cmd("Obsidian new_from_template conversation") end, { desc = "New [C]onversation" })
-                vim.keymap.set( "n", "<leader>oce", function() vim.cmd("Obsidian new_from_template email") end, { desc = "New [E]mail" })
-                vim.keymap.set( "n", "<leader>oci", function() vim.cmd("Obsidian new_from_template interview") end, { desc = "New [I]nterview" })
-                vim.keymap.set( "n", "<leader>ocm", function() vim.cmd("Obsidian new_from_template meeting") end, { desc = "New [M]eeting" })
-                vim.keymap.set( "n", "<leader>ocp", function() vim.cmd("Obsidian new_from_template phonecall") end, { desc = "New [P]honeCall" })
-                vim.keymap.set( "n", "<leader>oma", function() vim.cmd("Obsidian new_from_template article") end, { desc = "New [A]rticle" })
-                vim.keymap.set( "n", "<leader>omt", function() vim.cmd("Obsidian new_from_template boardgame") end, { desc = "New [T]abletop/board game" })
-                vim.keymap.set( "n", "<leader>omb", function() vim.cmd("Obsidian new_from_template book") end, { desc = "New [B]ook" })
-                vim.keymap.set( "n", "<leader>omm", function() vim.cmd("Obsidian new_from_template movie") end, { desc = "New [M]ovie" })
-                vim.keymap.set( "n", "<leader>oms", function() vim.cmd("Obsidian new_from_template show") end, { desc = "New [S]how" })
-                vim.keymap.set( "n", "<leader>omv", function() vim.cmd("Obsidian new_from_template video") end, { desc = "New [V]ideo" })
-                vim.keymap.set( "n", "<leader>omg", function() vim.cmd("Obsidian new_from_template videogame") end, { desc = "New Video [G]ame" })
+                vim.keymap.set("n", "<leader>op", function() newFromTemplate("contact") end, { desc = "New [P]erson" })
+                vim.keymap.set("n", "<leader>oi", function() newFromTemplate("idea") end, { desc = "New [I]dea" })
+                vim.keymap.set("n", "<leader>or", function() newFromTemplate("recipe") end, { desc = "New [R]ecipe" })
+                vim.keymap.set("n", "<leader>ot", function() newFromTemplate("topic") end, { desc = "New [T]opic" })
+                vim.keymap.set("n", "<leader>occ", function() newFromTemplate("conversation") end, { desc = "New [C]onversation" })
+                vim.keymap.set("n", "<leader>oce", function() newFromTemplate("email") end, { desc = "New [E]mail" })
+                vim.keymap.set("n", "<leader>oci", function() newFromTemplate("interview") end, { desc = "New [I]nterview" })
+                vim.keymap.set("n", "<leader>ocm", function() newFromTemplate("meeting") end, { desc = "New [M]eeting" })
+                vim.keymap.set("n", "<leader>ocp", function() newFromTemplate("phonecall") end, { desc = "New [P]honeCall" })
+                vim.keymap.set("n", "<leader>oma", function() newFromTemplate("article") end, { desc = "New [A]rticle" })
+                vim.keymap.set("n", "<leader>omt", function() newFromTemplate("boardgame") end, { desc = "New [T]abletop/board game" })
+                vim.keymap.set("n", "<leader>omb", function() newFromTemplate("book") end, { desc = "New [B]ook" })
+                vim.keymap.set("n", "<leader>omm", function() newFromTemplate("movie") end, { desc = "New [M]ovie" })
+                vim.keymap.set("n", "<leader>oms", function() newFromTemplate("show") end, { desc = "New [S]how" })
+                vim.keymap.set("n", "<leader>omv", function() newFromTemplate("video") end, { desc = "New [V]ideo" })
+                vim.keymap.set("n", "<leader>omg", function() newFromTemplate("videogame") end, { desc = "New Video [G]ame" })
             end,
-            enter_note = function(client, note)
+            enter_note = function(note)
                 local bufnr = note.bufnr
                 vim.keymap.set("n", "<leader>T", function()
                     local success, tags = pcall(getInput, "Enter space separated Tags:", { completion = "file" })
@@ -175,8 +171,7 @@ return {
                             note:add_tag(trimmed)
                         end
                     end
-                    -- note:add_tag(tag)
-                    note:force_update_frontmatter(bufnr)
+                    note:update_frontmatter(bufnr)
                 end, { buffer = bufnr, remap = false, desc = "Add Tags" })
             end,
         },
